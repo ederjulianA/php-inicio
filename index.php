de232671395e621f241e961a12aec8c01eb9189b
ederjulianA <ederalvarez2091057@gmail.com>
2014-03-22 17:21:04 -0500

    index de prueba

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	$var = "Eder";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Heroku AND php</title>
+</head>
+<body>
+<h1>APP corriendo desde heroku</h1>
+<p><?php echo $var;  ?></p>
+
+
+</body>
+</html>
